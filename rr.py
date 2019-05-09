4cd3fd323f0629792fa25db1972e8e88bd48f9e3
Logan <vaserk@gmail.com>
2019-05-09 22:56:47 +0800

    deside range

--- rr.py
+++ rr.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint (1, 100)
+start = input ('請輸入開始範圍： ')
+end  = input ('請輸入結束範圍： ')
+start = int (start)
+end  = int (end)
+r = random.randint (start, end)
 while True:
 	x = input ('請輸入數字： ')
 	if int(x) == r:
